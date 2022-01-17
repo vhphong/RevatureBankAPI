9ba39f786f9c1ac05854d47ced5d044d8d92c7b0
vhphong <vhphong@gmail.com>
2022-01-17 12:44:17 -0500

    added testCreateCustomer

--- src/test/java/CustomerRepositoryTests.java
+++ src/test/java/CustomerRepositoryTests.java
@@ -1,5 +1,6 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
@@ -15,6 +16,7 @@ public class CustomerRepositoryTests {
 	private CustomerRepository customerRepository;
 
 	// test methods placed here
+	@Test
 	public void testCreateCustomer() {
 
 		Customer customer = new Customer();
