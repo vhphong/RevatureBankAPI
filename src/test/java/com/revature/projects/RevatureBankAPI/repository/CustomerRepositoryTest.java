7140de47dd8841aff3e0713e0f8436cb307d2fdf
vhphong <vhphong@gmail.com>
2022-01-18 00:21:38 -0500

    added testFindTransactionByTransactionId

--- src/test/java/com/revature/projects/RevatureBankAPI/repository/CustomerRepositoryTest.java
+++ src/test/java/com/revature/projects/RevatureBankAPI/repository/CustomerRepositoryTest.java
@@ -30,7 +30,6 @@ public class CustomerRepositoryTest {
 
 	}
 
-
 	@Test
 	public void testFindCustomerByCustomerId() {
 
--- src/test/java/com/revature/projects/RevatureBankAPI/repository/TransactionRepositoryTest.java
+++ src/test/java/com/revature/projects/RevatureBankAPI/repository/TransactionRepositoryTest.java
@@ -1,37 +1,54 @@
 package com.revature.projects.RevatureBankAPI.repository;
 
-import com.revature.projects.RevatureBankAPI.model.Transaction;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.revature.projects.RevatureBankAPI.model.Transaction;
 
 @DataJpaTest
 public class TransactionRepositoryTest {
 
-    @Autowired
-    private TestEntityManager entityManager;
+	@Autowired
+	private TestEntityManager entityManager;
 
-    @Autowired
-    private TransactionRepository transactionRepository;
+	@Autowired
+	private TransactionRepository transactionRepository;
 
-    @Test
-    public void testCreateTransaction() {
+	@Test
+	public void testCreateTransaction() {
 
-        Transaction transaction = new Transaction();
-        transaction.setType("saving");
-        transaction.setAmount(4563.12);
-        transaction.setStatus("active");
-        transaction.setCustId(Long.valueOf(1));
+		Transaction transaction = new Transaction();
+		transaction.setType("saving");
+		transaction.setAmount(4563.12);
+		transaction.setStatus("active");
+		transaction.setCustId(Long.valueOf(1));
 
-        Transaction savedTransaction = transactionRepository.save(transaction);
-        Transaction expectedTransaction = entityManager.find(Transaction.class, savedTransaction.getTransactionId());
+		Transaction savedTransaction = transactionRepository.save(transaction);
+		Transaction expectedTransaction = entityManager.find(Transaction.class, savedTransaction.getTransactionId());
 
-        assertNotNull(expectedTransaction);
+		assertNotNull(expectedTransaction);
 
-    }
+	}
 
+	@Test
+	public void testFindTransactionByTransactionId() {
+
+		Transaction transaction = new Transaction();
+		transaction.setType("saving");
+		transaction.setAmount(4563.12);
+		transaction.setStatus("active");
+		transaction.setCustId(Long.valueOf(1));
+
+		Transaction savedTransaction = transactionRepository.save(transaction);
+
+		Boolean isExisted = transactionRepository.existsById(savedTransaction.getTransactionId());
+
+		assertThat(isExisted).isEqualTo(true);
+	}
 
 }
