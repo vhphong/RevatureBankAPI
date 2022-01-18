3d067e82f8978584478357d5913d09f3823c86c3
vhphong <vhphong@gmail.com>
2022-01-17 23:08:00 -0500

    added testCreateCustomer

--- src/test/java/com/revature/projects/RevatureBankAPI/repository/CustomerRepositoryTest.java
+++ src/test/java/com/revature/projects/RevatureBankAPI/repository/CustomerRepositoryTest.java
@@ -4,6 +4,7 @@ import com.revature.projects.RevatureBankAPI.model.Customer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,11 +12,17 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CustomerRepositoryTest {
 
     @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
     private CustomerRepository customerRepository;
 
     @Test
     public void testCreateCustomer() {
         Customer customer  = new Customer("TestName","testemail@email.com", "testpassword");
-        customerRepository.save(customer);
+        Customer savedCustomer = customerRepository.save(customer);
+        Customer expectedCustomer = entityManager.find(Customer.class, savedCustomer.getId());
+
+        assertNotNull(expectedCustomer);
     }
 }
