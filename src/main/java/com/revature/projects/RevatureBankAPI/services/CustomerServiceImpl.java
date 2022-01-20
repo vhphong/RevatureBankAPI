81a55bf2d74798c2da298f02af5838de068bc7d8
vhphong <vhphong@gmail.com>
2022-01-20 00:08:06 -0500

    edited CustomerServiceImpl

--- src/main/java/com/revature/projects/RevatureBankAPI/services/CustomerServiceImpl.java
+++ src/main/java/com/revature/projects/RevatureBankAPI/services/CustomerServiceImpl.java
@@ -5,6 +5,7 @@ import com.revature.projects.RevatureBankAPI.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Optional;
 
 public class CustomerServiceImpl implements CustomerService {
 
@@ -13,27 +14,49 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public List<Customer> findAllCustomers() {
-        return null;
+        return (List<Customer>) customerRepository.findAll();
     }
 
     @Override
     public Customer findCustomerById(Long id) {
-        return null;
+        Optional<Customer> result = customerRepository.findById(id);
+
+        if (result.isPresent()) {
+            return result.get();
+        } else {
+            return null;
+        }
     }
 
     @Override
     public Customer insertCustomer(Customer c) {
-        return null;
+        return customerRepository.save(c);
     }
 
     @Override
     public boolean modifyCustomer(Customer c) {
-        return false;
+        try {
+            customerRepository.save(c);
+
+            return true;
+        } catch (Exception error) {
+            System.out.println(error.getMessage());
+
+            return false;
+        }
     }
 
     @Override
     public boolean removeCustomer(Long id) {
-        return false;
+        try {
+            customerRepository.deleteById(id);
+
+            return true;
+        } catch (Exception error) {
+            System.out.println(error.getMessage());
+
+            return false;
+        }
     }
 
 }
