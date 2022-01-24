c594fff88d3188d280e45ca73a6d228e6599cdd8
vhphong <vhphong@gmail.com>
2022-01-23 20:55:58 -0500

    added: getAllAccounts controller

--- src/main/java/com/revature/projects/controllers/AccountController.java
+++ src/main/java/com/revature/projects/controllers/AccountController.java
@@ -5,10 +5,9 @@ import com.revature.projects.models.Account;
 import com.revature.projects.services.AccountService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -43,5 +42,9 @@ public class AccountController {
     }
 
 
-
+    // build get all accounts REST API
+    @GetMapping("accounts")
+    public List<Account> getAllAccounts() {
+        return accountService.listAllAccounts();
+    }
 }
