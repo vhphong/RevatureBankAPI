4e54b92944492fbc0d9babb29267da9b09b44419
vhphong <vhphong@gmail.com>
2022-01-23 20:33:16 -0500

    added: saveAccount controller

---
+++ src/main/java/com/revature/projects/controllers/AccountController.java
@@ -0,0 +1,47 @@
+package com.revature.projects.controllers;
+
+import com.revature.projects.exceptions.BadRequestException;
+import com.revature.projects.models.Account;
+import com.revature.projects.services.AccountService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/RevBankAPI/v2/")
+public class AccountController {
+
+    private AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        super();
+        this.accountService = accountService;
+    }
+
+
+    // build create account REST API
+    /*
+        {
+            "custId": 1,
+            "balance": 0.00,
+            "dateOfOpening": "2020-11-22",
+            "type": "debit",
+            "isActive": true
+        }
+    */
+    @PostMapping("accounts")
+    public ResponseEntity<Account> saveAccount(@RequestBody Account account) {
+        if (account != null) {
+            return new ResponseEntity<Account>(accountService.insertAccount(account), HttpStatus.CREATED);
+        } else {
+            throw new BadRequestException("Request body does not contain customer data");
+        }
+    }
+
+
+
+}
