ff7cb40f7124dff15a9cb8254a900b5bf199fde4
vhphong <vhphong@gmail.com>
2022-03-01 21:23:52 -0500

    added: CustomerServiceTest with listAllCustomersTest

--- src/test/java/com/revature/projects/services/CustomerServiceTest.java
+++ src/test/java/com/revature/projects/services/CustomerServiceTest.java
@@ -1,26 +1,90 @@
 package com.revature.projects.services;
 
+import com.revature.projects.models.Customer;
 import com.revature.projects.repositories.CustomerRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
-public class CustomerServiceTest {
+import static org.mockito.Mockito.verify;
 
-    @Autowired
-    private CustomerService customerService;
+public class CustomerServiceTest<customerService> {
 
-    @Autowired
+    @Mock
     private CustomerRepository customerRepository;
 
+    @Mock
+    private AutoCloseable autoCloseable;
+
+    @Mock
+    private CustomerService customerService;
+
+
+    @BeforeEach
+    void setUp() {
+        AutoCloseable autoCloseable = MockitoAnnotations.openMocks(this);
+        customerService = new CustomerService() {
+            @Override
+            public Customer insertCustomer(Customer customer) {
+                return null;
+            }
+
+            @Override
+            public List<Customer> listAllCustomers() {
+                return customerRepository.findAll();
+            }
+
+            @Override
+            public Customer listCustomerById(long id) {
+                return null;
+            }
+
+            @Override
+            public List<Customer> listAllCustomersByName(String name) {
+                return null;
+            }
+
+            @Override
+            public List<Customer> retrieveCustomerByNameAndEmail(String name, String email) {
+                return null;
+            }
+
+            @Override
+            public Customer modifyCustomer(Customer customer, long id) {
+                return null;
+            }
+
+            @Override
+            public boolean removeCustomer(long id) {
+                return false;
+            }
+
+            @Override
+            public String welcomeCustomer() {
+                return null;
+            }
+        };
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        autoCloseable.close();
+    }
 
     @Test
     void insertCustomer() {
     }
 
     @Test
-    void listAllCustomers() {
+    void listAllCustomersTest() {
+        // when
+        customerService.listAllCustomers();
+        // then
+        verify(customerRepository).findAll();
     }
 
     @Test
