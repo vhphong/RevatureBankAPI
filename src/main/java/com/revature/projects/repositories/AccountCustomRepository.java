ab5bea0c041e11f9e7f2c77305bf2f8abf00abdc
vhphong <vhphong@gmail.com>
2022-11-01 17:07:38 -0400

    added disableAccount

--- src/main/java/com/revature/projects/repositories/AccountCustomRepository.java
+++ src/main/java/com/revature/projects/repositories/AccountCustomRepository.java
@@ -15,6 +15,5 @@ public interface AccountCustomRepository {
     List<Account> findAccountByCustomerName(String customerNameInput);
 
     List<Account> findAccountByAccountActiveStatus(int accountActiveStatusInput);
-
-//    void freezeAccount(long accountId);
+    
 }
